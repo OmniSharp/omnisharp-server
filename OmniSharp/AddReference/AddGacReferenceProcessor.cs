1711727565e90cb9a0130cef8eaf68d4d88cca00
Gareth Williams <gw@gdoubleu.co.uk>
2013-05-24 08:45:20 +0100

    First cut at adding a gac reference

---
+++ OmniSharp/AddReference/AddGacReferenceProcessor.cs
@@ -0,0 +1,56 @@
+﻿using System.Linq;
+using System.Xml.Linq;
+using OmniSharp.Solution;
+
+namespace OmniSharp.AddReference
+{
+    public class AddGacReferenceProcessor : ReferenceProcessorBase, IReferenceProcessor
+    {
+        public AddReferenceResponse AddReference(IProject project, string reference)
+        {
+            var response = new AddReferenceResponse();
+
+            var projectXml = project.AsXml();
+
+            var compilationNodes = GetReferenceNodes(projectXml, "Reference");
+
+            var referenceAlreadyAdded = compilationNodes.Any(n => n.Attribute("Include").Value.Equals(reference));
+
+            var fileReferenceNode = CreateReferenceNode(reference);
+
+            if (!referenceAlreadyAdded)
+            {
+                if (compilationNodes.Count > 0)
+                {
+                    compilationNodes.First().Parent.Add(fileReferenceNode);
+                }
+                else
+                {
+                    var projectItemGroup = new XElement(MsBuildNameSpace + "ItemGroup");
+                    projectItemGroup.Add(fileReferenceNode);
+                    projectXml.Element(MsBuildNameSpace + "Project").Add(projectItemGroup);
+                }
+
+                project.AddReference(reference.FixPath());
+                project.Save(projectXml);
+
+                response.Message = string.Format("Reference to {0} added successfully", reference);
+            }
+            else
+            {
+                response.Message = "Reference already added";
+            }
+
+            return response;
+        }
+
+        XElement CreateReferenceNode(string referenceName)
+        {
+            var projectReferenceNode =
+                new XElement(MsBuildNameSpace + "Reference",
+                    new XAttribute("Include", referenceName));
+
+            return projectReferenceNode;
+        }
+    }
+}
