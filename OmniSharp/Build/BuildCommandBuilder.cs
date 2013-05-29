569b06c095a8eb1bb21a8e91afe6169c10133734
nosami <jasonimison@gmail.com>
2013-05-29 19:17:30 +0100

    added async build comamnd

---
+++ OmniSharp/Build/BuildCommandBuilder.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.IO;
+using OmniSharp.Solution;
+
+namespace OmniSharp.Build
+{
+    public class BuildCommandBuilder
+    {
+        private readonly ISolution _solution;
+        private static bool IsUnix
+        {
+            get
+            {
+                var p = (int)Environment.OSVersion.Platform;
+                return (p == 4) || (p == 6) || (p == 128);
+            }
+        }
+
+        public BuildCommandBuilder(ISolution solution)
+        {
+            _solution = solution;
+        }
+
+        public string Executable
+        {
+            get
+            {
+                return IsUnix
+                           ? "xbuild"
+                           : Path.Combine(System.Runtime.InteropServices.RuntimeEnvironment.GetRuntimeDirectory(), "Msbuild.exe");
+            }
+        }
+
+        public string Arguments
+        {
+            get { return IsUnix ? "" : "/m " + "/nologo /v:q /property:GenerateFullPaths=true \"" + _solution.FileName + "\""; }
+        }
+    }
+}
