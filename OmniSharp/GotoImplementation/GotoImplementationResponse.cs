f541236cd8c31859371dcd4850d434967c83f692
Mika Vilpas <mika.vilpas@gmail.com>
2013-06-28 00:14:22 +0300

    Allow constructing a GotoImplementationResponse with QuickFixes

--- OmniSharp/GotoImplementation/GotoImplementationResponse.cs
+++ OmniSharp/GotoImplementation/GotoImplementationResponse.cs
@@ -1,9 +1,21 @@
 ﻿using System.Collections.Generic;
+using System.Linq;
+using OmniSharp.Common;
 
 namespace OmniSharp.GotoImplementation
 {
     public class GotoImplementationResponse
     {
+        public GotoImplementationResponse() {}
+
+        public GotoImplementationResponse
+            (IEnumerable<QuickFix> quickFixes) {
+
+            this.Locations = quickFixes
+                .Select(qf => qf.ConvertToLocation())
+                .ToArray();
+        }
+
         public IEnumerable<Location> Locations = new List<Location>();
     }
 }
