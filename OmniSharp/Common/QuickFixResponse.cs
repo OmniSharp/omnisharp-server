6bf04b5920b528625d1f5e0f5e3a2e58572b27b0
Mika Vilpas <mika.vilpas@gmail.com>
2013-07-01 09:43:03 +0300

    Add QuickFixResponse base class

---
+++ OmniSharp/Common/QuickFixResponse.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections.Generic;
+using OmniSharp.Common;
+
+namespace OmniSharp.Common {
+
+    /// <summary>
+    ///   Base class for QuickFix-centered responses.
+    /// </summary>
+    public abstract class QuickFixResponse {
+        public QuickFixResponse() {}
+
+        public QuickFixResponse(IEnumerable<QuickFix> quickFixes) {
+            this.QuickFixes = quickFixes;
+        }
+
+        protected IEnumerable<QuickFix> QuickFixes { get; set; }
+    }
+}
