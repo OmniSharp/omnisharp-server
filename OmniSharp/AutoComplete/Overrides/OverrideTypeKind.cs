ae19763ef8c7b0cb37767c4f226bf79c3c2b6fec
Mika Vilpas <mika.vilpas@gmail.com>
2013-07-02 14:38:14 +0300

    Add OverrideTypeKind enum

---
+++ OmniSharp/AutoComplete/Overrides/OverrideTypeKind.cs
@@ -0,0 +1,12 @@
+namespace OmniSharp.AutoComplete.Overrides {
+
+    /// <summary>
+    ///   Specifies a kind of type that may be queried from
+    ///   AutoCompleteOverrideHandler.
+    /// </summary>
+    public enum OverrideTypeKind
+        { UnknownInvalidOption
+        , Method
+        , Property
+        , Event}
+}
