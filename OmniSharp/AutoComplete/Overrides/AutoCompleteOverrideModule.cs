e90cae7e8e19213649cf79745122f00318487a98
Mika Vilpas <mika.vilpas@gmail.com>
2013-07-03 16:06:49 +0300

    Add AutoCompleteOverrideModule

---
+++ OmniSharp/AutoComplete/Overrides/AutoCompleteOverrideModule.cs
@@ -0,0 +1,20 @@
+﻿using System.Linq;
+using Nancy;
+using Nancy.ModelBinding;
+
+namespace OmniSharp.AutoComplete.Overrides {
+    public class AutoCompleteOverrideModule : NancyModule {
+
+        public AutoCompleteOverrideModule
+            (AutoCompleteOverrideHandler overrideHandler) {
+
+            Post["/autocompleteoverrides"] = x =>
+                {
+                    var req = this.Bind<AutoCompleteRequest>();
+                    var completions = overrideHandler.GetOverrideTargets(req);
+                    return Response.AsJson(completions);
+                };
+        }
+
+    }
+}
