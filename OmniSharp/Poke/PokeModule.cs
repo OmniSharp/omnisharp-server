570830195bcb8760bc556332eb39162d8b77de38
Chiel92 <ctje92@gmail.com>
2013-07-08 10:35:20 +0200

    Poke module added

---
+++ OmniSharp/Poke/PokeModule.cs
@@ -0,0 +1,16 @@
+﻿using Nancy;
+using OmniSharp.Solution;
+
+namespace OmniSharp.PokeModule
+{
+    public class PokeModule : NancyModule
+    {
+        public PokeModule(ISolution solution)
+        {
+            Post["/poke"] = x =>
+                {
+                    return Response.AsJson(true);
+                };
+        }
+    }
+}
