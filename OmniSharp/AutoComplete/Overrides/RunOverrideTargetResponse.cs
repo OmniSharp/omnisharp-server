923096cca347b4f0d173c144a28497b500a19665
Mika Vilpas <mika.vilpas@gmail.com>
2013-07-10 21:40:27 +0300

    Implement RunOverrideTargetResponse

--- OmniSharp/AutoComplete/Overrides/RunOverrideTargetResponse.cs
+++ OmniSharp/AutoComplete/Overrides/RunOverrideTargetResponse.cs
@@ -1,6 +1,22 @@
 using System;
 using OmniSharp.Common;
+using OmniSharp.Rename;
 
 namespace OmniSharp.AutoComplete.Overrides {
-    public class RunOverrideTargetResponse {}
+    public class RunOverrideTargetResponse : ModifiedFileResponse {
+        public RunOverrideTargetResponse() {}
+        public RunOverrideTargetResponse
+            ( string fileName
+            , string buffer
+            , int    line
+            , int    column)
+            : base(fileName: fileName, buffer: buffer) {
+            this.Line = line;
+            this.Column = column;
+        }
+
+        public int Line {get; set;}
+        public int Column {get; set;}
+
+    }
 }
