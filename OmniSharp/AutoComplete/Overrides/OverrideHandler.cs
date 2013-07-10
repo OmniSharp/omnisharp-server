8f426d76b43ce104044387bad806fb0b2e40f162
Mika Vilpas <mika.vilpas@gmail.com>
2013-07-10 21:41:23 +0300

    Continue OverrideHandler implementation

--- OmniSharp/AutoComplete/Overrides/OverrideHandler.cs
+++ OmniSharp/AutoComplete/Overrides/OverrideHandler.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using ICSharpCode.NRefactory.Editor;
 using ICSharpCode.NRefactory.TypeSystem;
 using OmniSharp.AutoComplete;
 using OmniSharp.Common;
@@ -8,6 +9,8 @@ using OmniSharp.Parser;
 using OmniSharp.Refactoring;
 using OmniSharp.Solution;
 using Omnisharp.AutoComplete.Overrides;
+using ICSharpCode.NRefactory.CSharp;
+using ICSharpCode.NRefactory.CSharp.Refactoring;
 
 namespace OmniSharp.AutoComplete.Overrides {
     public class OverrideHandler {
@@ -36,7 +39,130 @@ namespace OmniSharp.AutoComplete.Overrides {
         /// </summary>
         public RunOverrideTargetResponse RunOverrideTarget
             (RunOverrideTargetRequest request) {
+            var overrideContext = new OverrideContext(request, this._parser);
+            var refactoringContext = OmniSharpRefactoringContext.GetContext
+                (overrideContext.BufferParser, request);
 
+            // TODO
+            // Try looking at NRefactory source to see whether
+            // something in that library knows how to create an
+            // EntityDeclaration (abstract, derives from
+            // AstNode. These can probably be formatted back to text.)
+            var memberToOverride =  overrideContext.GetOverridableMembers()
+                .First(ot => ot.ToString() == request.WordToComplete);;
+
+            Console.WriteLine("refactoringContext=" + refactoringContext);
+            Console.WriteLine("memberToOverride=" + memberToOverride);
+
+            var script = new OmniSharpScript(refactoringContext);
+            var newEditorContents = runOverrideTargetWorker
+                (memberToOverride, script);
+            Console.WriteLine("newEditorContents=" + newEditorContents);
+
+            // // From CreateMethodDeclarationAction
+            // // TODO there are a bunch of promising looking static
+            // //      methods in that class.
+            // script.InsertWithCursor
+            //     ( "THIS IS AN OPERATION"
+            //     , ICSharpCode.NRefactory.CSharp.Refactoring.Script.InsertPosition.After
+            //     , new[] {methodDeclaration});
+
+            // return new RunOverrideTargetResponse
+            //     ( fileName : script.CurrentDocument.FileName
+            //     , buffer   : script.CurrentDocument.Text);
+            return new RunOverrideTargetResponse
+                ( fileName : request.FileName
+                , buffer   : newEditorContents.Text
+                // TODO Set cursor to a reasonable location!
+                // This way the cursor is set to a location that might
+                // be offset by the amount of lines that are inserted
+                // in the overriding declaration.
+                , line     : request.Line
+                , column   : request.Column);
+
+        }
+
+        /// <summary>
+        ///   Creates an overriding declaration for the given IMember,
+        ///   which must be IMethod, IProperty or IEvent. Inserts this
+        ///   declaration with the given script.
+        /// </summary>
+        /// <remarks>
+        ///   Alters the given script. Returns its CurrentDocument
+        ///   property.
+        /// </remarks>
+        IDocument runOverrideTargetWorker
+            ( IMember         memberToOverride
+            , OmniSharpScript script) {
+
+            if (memberToOverride is IMethod) {
+                var methodDeclaration = GetOverrideDeclarationForMethod
+                    ((IMethod) memberToOverride);
+                script.InsertWithCursor
+                    ( "THIS IS AN OPERATION"
+                    , Script.InsertPosition.After
+                    , new[] {methodDeclaration});
+            }
+            else if (memberToOverride is IProperty) {
+                var propertyDeclaration = GetOverrideDeclarationForProperty
+                    ((IProperty) memberToOverride);
+                script.InsertWithCursor
+                    ( "THIS IS AN OPERATION"
+                    , Script.InsertPosition.After
+                    , new[] {propertyDeclaration});
+            }
+            else if (memberToOverride is IEvent) {
+                var eventDeclaration = GetOverrideDeclarationForEvent
+                    ((IEvent) memberToOverride);
+                script.InsertWithCursor
+                    ( "THIS IS AN OPERATION"
+                    , Script.InsertPosition.After
+                    , new[] {eventDeclaration});
+            }
+            else
+                throw new NotSupportedException
+                    ("Can only override IMethod / IProperty / IEvent");
+
+            return script.CurrentDocument;
+        }
+
+        static MethodDeclaration GetOverrideDeclarationForMethod
+            (IMethod iMethod) {
+
+            var invocationExpression = new InvocationExpression();
+                // ( methodName    : "base." + iMethod.Name
+                // , typeArguments : iMethod.TypeParameters
+                // , arguments     : iMethod.Parameters);
+
+            MethodDeclaration methodDeclaration = new MethodDeclaration()
+                { ReturnType = AstType.Create(iMethod.ReturnType.FullName)
+                , Name       = iMethod.Name
+                , Body       = new BlockStatement
+                    // TODO call base.foo() with same arguments
+                    {invocationExpression}};
+
+            //methodDeclaration.Parameters.AddRange(iMethod.Parameters);
+            // if (isStatic)
+            //     methodDeclaration.Modifiers |= Modifiers.Static;
+
+            return methodDeclaration;
+        }
+
+        static PropertyDeclaration GetOverrideDeclarationForProperty
+            (IProperty iProperty) {
+            throw new NotImplementedException();
+        }
+
+        static EventDeclaration GetOverrideDeclarationForEvent
+            (IEvent iEvent) {
+            throw new NotImplementedException();
+        }
+
+        /// <summary>
+        ///   Returns the type currently under the cursor in the given
+        ///   Request.
+        /// </summary>
+        public static OverrideContext GetCurrentType(Request request) {
             throw new NotImplementedException();
         }
 
