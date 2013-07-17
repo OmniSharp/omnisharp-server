0f29c3f95045414d9ff9b1cb8d6483fb0e0a42ee
Mika Vilpas <mika.vilpas@gmail.com>
2013-07-17 21:19:35 +0300

    Add features for navigating the AST in the current file

---
+++ OmniSharp/CurrentFileMembers/CurrentFileMembersAsTreeModule.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using Nancy;
+using Nancy.ModelBinding;
+
+namespace OmniSharp.CurrentFileMembers {
+    public class CurrentFileMembersAsTreeModule : NancyModule {
+        public CurrentFileMembersAsTreeModule
+            (CurrentFileMembersHandler handler) {
+
+            Post["/currentfilemembersastree"] = x =>
+            {
+                var req = this.Bind<CurrentFileMembersRequest>();
+                var members = handler.GetCurrentFileMembersAsTree(req);
+                return Response.AsJson(members);
+            };
+        }
+    }
+}
