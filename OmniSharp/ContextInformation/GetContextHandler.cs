baadab401d389c6c8a78cb33e4101b8fed1269a2
nosami <jasonimison@gmail.com>
2013-10-26 23:20:31 +0100

    added context info command for getting method info to test runner

---
+++ OmniSharp/ContextInformation/GetContextHandler.cs
@@ -0,0 +1,53 @@
+﻿using System.IO;
+using System.Linq;
+using ICSharpCode.NRefactory.CSharp;
+using OmniSharp.AutoComplete;
+using OmniSharp.Common;
+using OmniSharp.Parser;
+using OmniSharp.Solution;
+
+namespace OmniSharp.ContextInformation
+{
+    public class GetContextHandler
+    {
+        private readonly ISolution _solution;
+        private readonly BufferParser _parser;
+
+        public GetContextHandler(ISolution solution, BufferParser parser)
+        {
+            _solution = solution;
+            _parser = parser;
+        }
+
+        public GetContextResponse GetContextResponse(Request request)
+        {
+            string methodName = null;
+            string typeName = null;
+
+            var node = new BufferContext(request, _parser).NodeCurrentlyUnderCursor;
+            var method = (MethodDeclaration) node.AncestorsAndSelf.FirstOrDefault(n => n is MethodDeclaration);
+            if (method != null)
+                methodName = method.Name;
+
+            var type = (TypeDeclaration)node.AncestorsAndSelf.FirstOrDefault(n => n is TypeDeclaration);
+            var namespaceDeclaration = (NamespaceDeclaration)node.AncestorsAndSelf.FirstOrDefault(n => n is NamespaceDeclaration);
+
+            if (type != null)
+                typeName = type.Name;
+
+            if (namespaceDeclaration != null)
+                typeName = namespaceDeclaration.FullName + "." + typeName;
+
+            var project = _solution.ProjectContainingFile(request.FileName);
+            var directory = new FileInfo(project.FileName).Directory.GetDirectories("bin/Debug").First().FullName;
+            var assemblyName = Path.Combine(directory, project.ProjectContent.FullAssemblyName + ".dll");
+
+            return new GetContextResponse
+                {
+                    MethodName = methodName, 
+                    TypeName = typeName, 
+                    AssemblyName = "\"" + assemblyName + "\""
+                };
+        }
+    }
+}
---
+++ OmniSharp/ContextInformation/GetContextResponse.cs
@@ -0,0 +1,9 @@
+﻿namespace OmniSharp.ContextInformation
+{
+    public class GetContextResponse
+    {
+        public string MethodName { get; set; }
+        public string AssemblyName { get; set; }
+        public string TypeName { get; set; }
+    }
+}
