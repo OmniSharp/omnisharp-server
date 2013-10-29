d786501ad2acc2153983e7702b911e15881d207a
nosami <jasonimison@gmail.com>
2013-10-29 22:57:28 +0000

    more mono bug fixes

--- OmniSharp.Tests/AutoComplete/BugFixTests.cs
+++ OmniSharp.Tests/AutoComplete/BugFixTests.cs
@@ -31,7 +31,7 @@ namespace OmniSharp.Tests.AutoComplete
         int n;
         n.T$;
     }
-}").First().ShouldEqual("string ToString(string format)");
+}").First().ShouldStartWith("string ToString(");
         }
 
         [Test]
