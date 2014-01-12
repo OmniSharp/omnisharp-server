a59414ca8096b7a7db9028881d5e44d9a016e2ec
nosami <jasonimison@gmail.com>
2014-01-12 18:56:24 +0000

    add update buffer to prevent rename clobbering

---
+++ OmniSharp/UpdateBuffer/UpdateBufferModule.cs
@@ -0,0 +1,22 @@
+﻿using Nancy;
+using Nancy.ModelBinding;
+using OmniSharp.Parser;
+
+namespace OmniSharp.UpdateBuffer
+{
+	/// <summary>
+	/// Used to update OmniSharp's view of a buffer
+	/// </summary>
+	public class UpdateBufferModule : NancyModule
+    {
+		public UpdateBufferModule(BufferParser bufferParser)
+		{
+			Post["/updatebuffer"] = x =>
+        	{
+				var req = this.Bind<Common.Request>();
+				bufferParser.ParsedContent(req.Buffer, req.FileName);
+				return Response.AsJson(true);
+			};
+        }
+    }
+}
