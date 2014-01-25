8076ec172823821110b20b6c5d09514f4678712b
nosami <jasonimison@gmail.com>
2014-01-25 00:20:30 +0000

    adding get documentation command

--- OmniSharp/TypeLookup/TypeLookupRequest.cs
+++ OmniSharp/TypeLookup/TypeLookupRequest.cs
@@ -4,6 +4,17 @@ namespace OmniSharp.TypeLookup
 {
     public class TypeLookupRequest : Request
     {
-        public bool IncludeDocumentation = true;
+		public bool IncludeDocumentation
+		{
+			get
+			{
+				return includeDocumentation;
+			}
+			set
+			{
+				includeDocumentation = value;
+			}
+		}
+        bool includeDocumentation = true;
     }
 }
