85ca94350b8ad4371e98cf283bcfbddcb345daa0
nosami <jasonimison@gmail.com>
2014-01-31 21:12:07 +0000

    fixed code actions

--- OmniSharp/CodeActions/CodeActionProviders.cs
+++ OmniSharp/CodeActions/CodeActionProviders.cs
@@ -10,13 +10,15 @@ namespace OmniSharp.CodeActions
     {
         public IEnumerable<CodeActionProvider> GetProviders()
         {
-            var types = Assembly.GetAssembly(typeof(CodeActionProvider))
+            var types = Assembly.GetAssembly(typeof(UseVarKeywordAction))
                                 .GetTypes()
                                 .Where(t => typeof(CodeActionProvider).IsAssignableFrom(t));
 
             IEnumerable<CodeActionProvider> providers =
                 types
-                    .Where(type => !type.IsInterface && !type.ContainsGenericParameters) //TODO: handle providers with generic params 
+                    .Where(type => !type.IsInterface 
+                            && !type.IsAbstract
+                            && !type.ContainsGenericParameters) //TODO: handle providers with generic params 
                     .Select(type => (CodeActionProvider) Activator.CreateInstance(type));
 
             return providers;
