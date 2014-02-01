647ac1455b7bdcb2d099c86b1b7c664f89a487d7
nosami <jasonimison@gmail.com>
2014-02-01 01:11:03 +0000

    update codeformat to work with nrefactory's new CSharpFormatter

--- OmniSharp/CodeFormat/CodeFormatHandler.cs
+++ OmniSharp/CodeFormat/CodeFormatHandler.cs
@@ -1,9 +1,8 @@
 ﻿using System;
 using ICSharpCode.NRefactory.CSharp;
-
 namespace OmniSharp.CodeFormat
 {
-    public class CodeFormatHandler  
+    public class CodeFormatHandler
     {
         public CodeFormatResponse Format(CodeFormatRequest request)
         {
@@ -12,8 +11,13 @@ namespace OmniSharp.CodeFormat
             options.WrapLineLength = 80;
             options.TabsToSpaces = request.ExpandTab;
             var policy = FormattingOptionsFactory.CreateAllman();
-			var output = new CSharpFormatter (policy, options).Format (request.Buffer);
-			return new CodeFormatResponse(output);
+            policy.BlankLinesAfterUsings = 1;
+            policy.BlankLinesBetweenTypes = 1;
+            policy.BlankLinesBetweenMembers = 1;
+            var formatter = new CSharpFormatter(policy, options);
+            formatter.FormattingMode = FormattingMode.Intrusive;
+            var output = formatter.Format(request.Buffer);
+            return new CodeFormatResponse(output);
         }
     }
 }
