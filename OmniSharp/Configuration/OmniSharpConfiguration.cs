0eb8644f67c020fc570aaf6629beabb7654ac944
boris <boris.buliga@icloud.com>
2014-07-16 18:17:02 +0300

    add ability to choose one of standart formatting styles or to configure user's custom style

--- OmniSharp/Configuration/OmniSharpConfiguration.cs
+++ OmniSharp/Configuration/OmniSharpConfiguration.cs
@@ -17,7 +17,45 @@ namespace OmniSharp.Configuration
         public IEnumerable<string> IgnoredCodeIssues { get; set; }
         public TextEditorOptions TextEditorOptions { get; set; }
         public TestCommands TestCommands { get; set; }
-        public CSharpFormattingOptions CSharpFormattingOptions { get; set; }
+        public string CSharpFormattingOptionsName { get; set; }
+        private CSharpFormattingOptions _options;
+        public CSharpFormattingOptions CSharpFormattingOptions
+        {
+            set
+            {
+                switch (CSharpFormattingOptionsName)
+                {
+                    case "KRStyle":
+                        _options = FormattingOptionsFactory.CreateKRStyle();
+                        break;
+                    case "Allman":
+                        _options = FormattingOptionsFactory.CreateAllman();
+                        break;
+                    case "Empty":
+                        _options = FormattingOptionsFactory.CreateEmpty();
+                        break;
+                    case "GNU":
+                        _options = FormattingOptionsFactory.CreateGNU();
+                        break;
+                    case "Mono":
+                        _options = FormattingOptionsFactory.CreateMono();
+                        break;
+                    case "SharpDevelop":
+                        _options = FormattingOptionsFactory.CreateSharpDevelop();
+                        break;
+                    case "Whitesmiths":
+                        _options = FormattingOptionsFactory.CreateWhitesmiths();
+                        break;
+                    default:
+                        _options = value;
+                        break;
+                }
+            }
+            get
+            {
+                return _options;
+            }
+        }
         public bool? UseCygpath { get; set; }
         public PathMode? ClientPathMode { get; set; }
         public PathMode? ServerPathMode { get; set; }
