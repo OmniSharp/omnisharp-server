831fb5a615dd89062820d30dcc6ac0db95a9d47c
nosami <jasonimison@gmail.com>
2014-09-06 22:34:03 +0100

    added code check module

---
+++ OmniSharp/SyntaxErrors/CodeCheckHandler.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using OmniSharp.CodeIssues;
+using OmniSharp.Common;
+using OmniSharp.SemanticErrors;
+
+namespace OmniSharp.SyntaxErrors
+{
+    public class CodeCheckHandler
+    {
+        readonly SyntaxErrorsHandler _syntaxErrorsHandler;
+        readonly CodeIssuesHandler _codeIssuesHandler;
+        readonly SemanticErrorsHandler _semanticErrorsHandler;
+		
+        public CodeCheckHandler(SyntaxErrorsHandler syntaxErrorsHandler, CodeIssuesHandler codeIssuesHandler, SemanticErrorsHandler semanticErrorsHandler)
+        {
+            _semanticErrorsHandler = semanticErrorsHandler;
+            _codeIssuesHandler = codeIssuesHandler;
+            _syntaxErrorsHandler = syntaxErrorsHandler;
+        }
+
+        public IEnumerable<QuickFix> CodeCheck(Request request)
+        {
+            var errors = new List<QuickFix>();
+            errors.AddRange(_syntaxErrorsHandler.FindSyntaxErrors(request));
+            if (errors.Any())
+            {
+                return errors;
+            }
+            errors.AddRange(_semanticErrorsHandler.FindSemanticErrors(request));
+            if (errors.Any())
+            {
+                return errors;
+            }
+            errors.AddRange(_codeIssuesHandler.GetCodeIssues(request));
+			return errors;
+        }
+    }
+}
