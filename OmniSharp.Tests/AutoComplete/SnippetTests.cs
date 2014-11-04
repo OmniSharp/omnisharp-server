327818d20c223c5008119aaa8c4b1a4c7dae2246
nosami <jasonimison@gmail.com>
2014-11-04 21:39:38 +0000

    add test for constructor snippets re #126

--- OmniSharp.Tests/AutoComplete/SnippetTests.cs
+++ OmniSharp.Tests/AutoComplete/SnippetTests.cs
@@ -2,13 +2,13 @@ using NUnit.Framework;
 
 namespace OmniSharp.Tests.AutoComplete
 {
-	[TestFixture]
-	public class SnippetTests : CompletionTestBase
-	{
-		[Test]
-		public void Should_template_generic_type_argument()
-		{
-			SnippetFor(
+    [TestFixture]
+    public class SnippetTests : CompletionTestBase
+    {
+        [Test]
+        public void Should_template_generic_type_argument()
+        {
+            SnippetFor(
                 @"using System.Collections.Generic;
             public class Class1 {
                 public Class1()
@@ -16,13 +16,35 @@ namespace OmniSharp.Tests.AutoComplete
                     var l = new Lis$
                 }
             }")
-                .ShouldContain("List<${1:T}>()$0");	   
-		}
+                .ShouldContain("List<${1:T}>()$0");        
+        }
 
-		[Test]
-		public void Should_template_generic_type_arguments()
-		{
-			SnippetFor(
+        [Test]
+        public void Should_return_all_constructors()
+        {
+            SnippetFor(
+                @"public class MyClass {
+                            public MyClass() {}
+                            public MyClass(int param) {}
+                            public MyClass(string param) {}
+                        }
+
+                        public class Class2 {
+                            public Class2()
+                            {
+                                var c = new My$
+                            }
+                        }")
+                .ShouldContainOnly(
+                    "MyClass()$0",
+                    "MyClass(${1:int param})$0",
+                    "MyClass(${1:string param})$0");
+        }
+
+        [Test]
+        public void Should_template_generic_type_arguments()
+        {
+            SnippetFor(
                 @"using System.Collections.Generic;
             public class Class1 {
                 public Class1()
@@ -30,13 +52,13 @@ namespace OmniSharp.Tests.AutoComplete
                     var l = new Dict$
                 }
             }")
-                .ShouldContain("Dictionary<${1:TKey}, ${2:TValue}>()$0");	   
-		}
+                .ShouldContain("Dictionary<${1:TKey}, ${2:TValue}>()$0");      
+        }
 
-		[Test]
-		public void Should_template_parameter()
-		{
-			SnippetFor(
+        [Test]
+        public void Should_template_parameter()
+        {
+            SnippetFor(
                 @"using System.Collections.Generic;
             public class Class1 {
                 public Class1()
@@ -44,8 +66,8 @@ namespace OmniSharp.Tests.AutoComplete
                     var l = new Lis$
                 }
             }")
-                .ShouldContain("List<${1:T}>(${2:IEnumerable<T> collection})$0");	   
-		}
+                .ShouldContain("List<${1:T}>(${2:IEnumerable<T> collection})$0");      
+        }
 
         [Test]
         public void Should_return_method_type_arguments_snippets()
@@ -67,6 +89,6 @@ namespace OmniSharp.Tests.AutoComplete
                 .ShouldContain("Get<${1:SomeType}>()$0");
         }
 
-	}
+    }
     
 }
