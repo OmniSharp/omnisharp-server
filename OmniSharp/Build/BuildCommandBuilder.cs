60f4520669aa0b688183199e65bbd15b0791cac9
Astrophizz <astrophizz@gmail.com>
2014-12-01 16:29:21 -0600

    Use msbuild path from config.json if present
    Otherwise use runtime directory.

--- OmniSharp/Build/BuildCommandBuilder.cs
+++ OmniSharp/Build/BuildCommandBuilder.cs
@@ -6,10 +6,14 @@ namespace OmniSharp.Build
     public class BuildCommandBuilder
     {
         private readonly ISolution _solution;
+        private readonly OmniSharpConfiguration _config;
 
-        public BuildCommandBuilder(ISolution solution)
+        public BuildCommandBuilder(
+            ISolution solution,
+            OmniSharpConfiguration config)
         {
             _solution = solution;
+            _config = config;
         }
 
         public string Executable
@@ -18,8 +22,9 @@ namespace OmniSharp.Build
             {
                 return PlatformService.IsUnix
                     ? "xbuild"
-                    : Path.Combine(System.Runtime.InteropServices.RuntimeEnvironment.GetRuntimeDirectory(),
-                    "Msbuild.exe");
+                    : Path.Combine(
+                        _config.MSBuildPath ?? System.Runtime.InteropServices.RuntimeEnvironment.GetRuntimeDirectory(),
+                        "Msbuild.exe");
             }
         }
 
